f9dc5667c7fd67954a3d45a28b52d000dd4b1605
NastiaBur <s22d_burkova@179.ru>
2024-01-17 18:11:14 +0300

    added db_functions

--- db_functions.py
+++ db_functions.py
@@ -69,43 +69,154 @@ def receive_conn():
         print("Something is wrong")
         print(e)
 
-def add_user(db_user_id, db_status="start", db_coll_id="None_1", db_pair_n=1):
+def add_status(user_id, status="start", coll_id="None_1", pair_n=1):
     connection = receive_conn()
     try:
         with connection.cursor() as cursor:
-            insert_query = f"INSERT INTO `status` (user_id, status, coll_id, pair_n) VALUES{(db_user_id, db_status, db_coll_id, db_pair_n)}"
+            insert_query = f"INSERT INTO `status` (user_id, status, coll_id, pair_n) VALUES{(user_id, status, coll_id, pair_n)}"
             cursor.execute(insert_query)
             connection.commit()
     finally:
         connection.close()
 
-def change_status(db_user, db_status=None, db_coll_id=None, db_pair_n=None): 
+def change_status(user_id, status=None, coll_id=None, pair_n=None): 
     connection = receive_conn()
     try:
-        if db_status is not None:
+        if status is not None:
             with connection.cursor() as cursor:
-                update_query = f"UPDATE `status` SET status = '{db_status}' WHERE user_id = '{db_user}'"
+                update_query = f"UPDATE `status` SET status = '{status}' WHERE user_id = '{user_id}'"
                 cursor.execute(update_query)
                 connection.commit()
-        if db_coll_id is not None:
+        if coll_id is not None:
             with connection.cursor() as cursor:
-                update_query = f"UPDATE `status` SET coll_id = '{db_coll_id}' WHERE user_id = '{db_user}'"
+                update_query = f"UPDATE `status` SET coll_id = '{coll_id}' WHERE user_id = '{user_id}'"
                 cursor.execute(update_query)
                 connection.commit()
 
-        if db_pair_n is not None:
+        if pair_n is not None:
             with connection.cursor() as cursor:
-                update_query = f"UPDATE `status` SET pair_n = {db_pair_n} WHERE user_id = '{db_user}'"
+                update_query = f"UPDATE `status` SET pair_n = {pair_n} WHERE user_id = '{user_id}'"
                 cursor.execute(update_query)
                 connection.commit()
     finally:
         connection.close()
 
-def delete_user(db_user):
+def delete_status(user_id):
     connection = receive_conn()
     try:
         with connection.cursor() as cursor:
-            delete_query = f"DELETE FROM `status` WHERE user_id = '{db_user}'"
+            delete_query = f"DELETE FROM `status` WHERE user_id = '{user_id}'"
+            cursor.execute(delete_query)
+            connection.commit()
+    finally:
+        connection.close()
+
+
+def add_coll_info(coll_id, user_id, name):
+    connection = receive_conn()
+    try:
+        with connection.cursor() as cursor:
+            insert_query = f"INSERT INTO `collection_info` (coll_id, user_id, name) VALUES {(coll_id, user_id, name)}"
+            cursor.execute(insert_query)
+            connection.commit()
+    finally:
+        connection.close()
+
+def change_coll_info(coll_id, user_id=None, name=None): 
+    connection = receive_conn()
+    try:
+        if user_id is not None:
+            with connection.cursor() as cursor:
+                update_query = f"UPDATE `collection_info` SET user_id = '{user_id}' WHERE coll_id = '{coll_id}'"
+                cursor.execute(update_query)
+                connection.commit()
+        if name is not None:
+            with connection.cursor() as cursor:
+                update_query = f"UPDATE `status` SET name = '{name}' WHERE coll_id = '{coll_id}'"
+                cursor.execute(update_query)
+                connection.commit()
+    finally:
+        connection.close()
+
+def delete_coll_info(coll_id):
+    connection = receive_conn()
+    try:
+        with connection.cursor() as cursor:
+            delete_query = f"DELETE FROM `collection_info` WHERE coll_id = '{coll_id}'"
+            cursor.execute(delete_query)
+            connection.commit()
+    finally:
+        connection.close()
+
+
+def add_saved_coll(user_id, coll_id):
+    connection = receive_conn()
+    try:
+        with connection.cursor() as cursor:
+            insert_query = f"INSERT INTO `saved_collection` (user_id, coll_id) VALUES{(user_id, coll_id)}"
+            cursor.execute(insert_query)
+            connection.commit()
+    finally:
+        connection.close()
+
+def change_saved_coll(user_id, coll_id): 
+    connection = receive_conn()
+    try:
+         with connection.cursor() as cursor:
+            update_query = f"UPDATE `saved_collection` SET coll_id = '{coll_id}' WHERE user_id = '{user_id}'"
+            cursor.execute(update_query)
+            connection.commit()
+    finally:
+        connection.close()
+
+def delete_saved_coll(user_id, coll_id):
+    connection = receive_conn()
+    try:
+        with connection.cursor() as cursor:
+            delete_query = f"DELETE FROM `saved_collection` WHERE user_id = '{user_id}' AND coll_id = '{coll_id}'"
+            cursor.execute(delete_query)
+            connection.commit()
+    finally:
+        connection.close()
+
+
+def add_coll_content(coll_id, pair_n, eng_word, rus_word):
+    connection = receive_conn()
+    try:
+        with connection.cursor() as cursor:
+            insert_query = f"INSERT INTO `collection_content` (coll_id, pair_n, eng_word, rus_word) VALUES{(coll_id, pair_n, eng_word, rus_word)}"
+            cursor.execute(insert_query)
+            connection.commit()
+    finally:
+        connection.close()
+
+def change_coll_content(coll_id, pair_n = None, eng_word=None, rus_word=None): 
+    connection = receive_conn()
+    try:
+        if pair_n is not None:
+            with connection.cursor() as cursor:
+                update_query = f"UPDATE `collection_content` SET pair_n = '{pair_n}' WHERE coll_id = '{coll_id}'"
+                cursor.execute(update_query)
+                connection.commit()
+        if eng_word is not None:
+            with connection.cursor() as cursor:
+                update_query = f"UPDATE `collection_content` SET eng_word = '{eng_word}' WHERE coll_id = '{coll_id}'"
+                cursor.execute(update_query)
+                connection.commit()
+
+        if pair_n is not None:
+            with connection.cursor() as cursor:
+                update_query = f"UPDATE `collection_content` SET rus_word = {rus_word} WHERE coll_id = '{coll_id}'"
+                cursor.execute(update_query)
+                connection.commit()
+    finally:
+        connection.close()
+
+def delete_coll_content(coll_id):
+    connection = receive_conn()
+    try:
+        with connection.cursor() as cursor:
+            delete_query = f"DELETE FROM `collection_content` WHERE coll_id = '{coll_id}'"
             cursor.execute(delete_query)
             connection.commit()
     finally:
